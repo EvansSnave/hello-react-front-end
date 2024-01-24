aba57860f3721ac63631f763b383ff38ec707e8b
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2024-01-24 10:31:39 -0500

    frontend ready

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -1,7 +1,20 @@
-const Greetings = () => (
-  <div>
-    Hello
-  </div>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getGreetings } from '../redux/greeting/greetingSlice';
+
+const Greetings = () => {
+  const greeting = useSelector((state) => state.randomGreeting.greeting);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGreetings());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {greeting}
+    </div>
+  );
+};
 
 export default Greetings;
